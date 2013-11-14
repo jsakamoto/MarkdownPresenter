b3415332b9942e63fcd49e1a0c21be92240fbd34
Tomoatsu Shimada <shimada@walbrix.com>
2013-11-14 19:06:45 +0900

    Some shortcuts applied

--- markdown-presenter-bootstrap.py
+++ markdown-presenter-bootstrap.py
@@ -1,34 +1,26 @@
 import webbrowser
 import SimpleHTTPServer
-import thread
-import time
+import threading
 import urllib
 from zipfile import ZipFile
 import os
 
 tagname = "1.0"
 zipname = "markdown-presenter-" + tagname + ".zip"
-zippath = "./" + zipname
 url = "https://github.com/jsakamoto/MarkdownPresenter/releases/download/v." + tagname + "/" + zipname
 
 # download zip archive of Markdown Presenter.
-urllib.urlretrieve(url, zippath)
+zippath = urllib.urlretrieve(url)[0] # download a zip file as a randomly named tempfile
 
-# ectract the zip.
-zfile = ZipFile(zippath)
-zfile.extractall(".")
-zfile.close()
+# extract the zip.
+ZipFile(zippath).extractall()
 
 # clean up zip.
 os.remove(zippath)
 
 # launch default web browser to open Markdown Presenter
 # after one shot timer to wait for warming up HTTP daemon.
-def launch():
-  time.sleep(1)
-  webbrowser.open("http://localhost:8000/Presenter.html")
-
-thread.start_new_thread(launch, ())
+threading.Timer(1.0, webbrowser.open, ["http://localhost:8000/Presenter.html"]).start()
 
 # start mini HTTP daemon.
 SimpleHTTPServer.test()
